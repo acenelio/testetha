1694ddee9ea23b0a6c680733299e67829fb3f997
Thauane <thataspg@gmail.com>
2016-09-26 09:28:27 -0300

    Mudando de Dobro para Triplo

--- Aulaxadrez/Program.cs
+++ Aulaxadrez/Program.cs
@@ -10,9 +10,9 @@ namespace Aulaxadrez
 
             int x = int.Parse(Console.ReadLine());
 
-            int y = 2 * x;
+            int y = 3 * x;
 
-            Console.WriteLine("DOBRO = " + y);
+            Console.WriteLine("TRIPLO = " + y);
 
             Console.ReadLine();
         }
