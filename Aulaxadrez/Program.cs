deb5ba70012e3aca9ee032c59a1232a401d1f7ef
Thauane <thataspg@gmail.com>
2016-09-26 09:21:35 -0300

    primeiro commit

---
+++ Aulaxadrez/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aulaxadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor inteiro: ");
+
+            int x = int.Parse(Console.ReadLine());
+
+            int y = 2 * x;
+
+            Console.WriteLine("DOBRO = " + y);
+
+            Console.ReadLine();
+        }
+    }
+}
