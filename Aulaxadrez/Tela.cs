ed6fae958a636fd604545d6ddbef3bcda150bb64
Thauane <thataspg@gmail.com>
2016-10-03 09:56:34 -0300

    Melhorando Imlementação da Impressao do Tabuleiro

--- Aulaxadrez/Tela.cs
+++ Aulaxadrez/Tela.cs
@@ -9,6 +9,7 @@ namespace Aulaxadrez
         {
             for ( int i=0; i<tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for ( int j=0; j<tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -17,11 +18,31 @@ namespace Aulaxadrez
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
                     }
                 }
                 Console.WriteLine();
             }
+
+            Console.WriteLine("  a b c d e f g h");
         }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.branca)
+            {
+                Console.Write(peca);
+            }
+            
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            } 
+        }
+        
+
     }
 }
