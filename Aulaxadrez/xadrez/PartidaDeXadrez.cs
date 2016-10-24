7c7f8e23afe1344f8d341ae647fbabe5c1bf72c1
Thauane <thataspg@gmail.com>
2016-10-24 11:19:47 -0200

    Turnos

--- Aulaxadrez/xadrez/PartidaDeXadrez.cs
+++ Aulaxadrez/xadrez/PartidaDeXadrez.cs
@@ -6,8 +6,8 @@ namespace xadrez
     {
 
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set; }
         public bool terminada { get; private set; } 
 
 
@@ -16,6 +16,7 @@ namespace xadrez
             tab = new Tabuleiro(8, 8);
             turno = 1;
             jogadorAtual = Cor.branca;
+            terminada = false;
             colocarPecas();
         }
 
@@ -26,6 +27,38 @@ namespace xadrez
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
 
+        }
+
+        public void realizaJogada(Posicao  origem, Posicao destino)
+        {
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador()
+        }
+
+        public void validarposicaoDeOrigem(Posicao pos) {
+            if (tab.peca(pos) == null) {
+                throw new TabuleiroException("Não existe peça na posição de origem escolhida!");
+            }
+            if (jogadorAtual != tab.peca(pos).cor) {
+                throw new TabuleiroException("A peça de origem escolhida não é sua!");
+            }
+            if (tab.peca(pos) == null) {
+                throw new TabuleiroException("Não há moimentos possiveis para a peça de origem escolhida!");
+            }
+        }
+
+
+        private void mudaJogador()
+        {
+            if ( jogadorAtual == Cor.branca)
+            {
+                jogadorAtual = Cor.preta;
+            }
+            else
+            {
+                jogadorAtual = Cor.branca;
+            }
         }
 
         private void colocarPecas()
